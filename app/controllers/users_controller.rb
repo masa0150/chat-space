d847fe84a8936cd30e4ce692a60fb62cf228eff8
masa0150 <masa98989801@yahoo.co.jp>
2020-02-02 16:35:47 +0900

    ChatSpaceの不備の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
 
   def index
-    return nil if params[:keyword] == ""
-    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
-    respond_to do |format|
-      format.html
-      format.json
+
+    #キーワードがなければnilを返す
+      return nil if params[:keyword] == ""
+    
+    #その文字を含むユーザーを探してきて、@usersに代入、ただしログインしている自分は除く。10人まで。
+      @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+      respond_to do |format|
+        format.html
+        format.json
+      end
     end
-  end
 
   def edit
   end
